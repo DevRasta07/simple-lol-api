66e6b26360df9003ddf93e81c21011a4b3a9cc35
Bernardo Euler <rasta.dev07@gmail.com>
2022-04-24 03:50:09 -0300

    Adding accounts endpoints

---
+++ src/routes/accounts.js
@@ -0,0 +1,28 @@
+const router = require("express").Router()
+const { default: axios } = require("axios")
+const API_KEY = "RGAPI-55c7270e-5b45-4260-b155-24657ef83aca"
+
+router.get("/:region/accounts/:method/:key1/:key2?", async (req, res) => {
+    const { region, method, key1, key2 } = req.params
+    let url
+
+    if (method === "by-riot-id") {
+        url = `https://${region}.api.riotgames.com/riot/account/v1/accounts/${method}/${key1}/${key2}?api_key=${API_KEY}`
+    }
+
+    else {
+        url = `https://${region}.api.riotgames.com/riot/account/v1/accounts/${method}/${key1}?api_key=${API_KEY}`    
+    }
+
+    try {
+        const { data: summonerData } = await axios.get(url)
+        return res.json(summonerData)
+    }
+
+    catch (err) {
+        if (!err.response) return res.sendStatus(404)
+        return res.status(404).send(err.response.data.status.message)
+    }
+})
+
+module.exports = router
--- src/server.js
+++ src/server.js
@@ -4,10 +4,12 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(cors())
-app.use("/", require("./routes/champions.js"))
 
 app.get("/", (req, res) => {
     res.send()
 })
+
+app.use("/", require("./routes/champions"))
+app.use("/", require("./routes/accounts"))
 
 app.listen(PORT)
